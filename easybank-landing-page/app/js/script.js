de10c7319febfd8307bdc4db8f912437c0ac4258
Serhii <malgothsm@gmail.com>
2021-01-11 17:06:19 +0200

    Done animation

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,10 +1,15 @@
 const bttHamb = document.querySelector('#bttHamb');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 bttHamb.addEventListener('click', function() {
   if (header.classList.contains('open')) {
     header.classList.remove('open');
+    overlay.classList.remove('f-in');
+    overlay.classList.add('f-out');
   } else {
     header.classList.add('open');
+    overlay.classList.remove('f-out');
+    overlay.classList.add('f-in');
   }
 } );
