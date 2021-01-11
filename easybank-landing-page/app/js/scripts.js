fdbd99420180ff3a55e8c0fc8c3f349e2367f394
Serhii <malgothsm@gmail.com>
2021-01-11 22:35:26 +0200

    Halfway there. Done with animations

--- easybank-landing-page/app/js/scripts.js
+++ easybank-landing-page/app/js/scripts.js
@@ -2,12 +2,18 @@ const bttHamb = document.querySelector('#bttHamb');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadings = document.querySelectorAll('.fading');
+const body = document.querySelector('body');
 bttHamb.addEventListener('click', function() {
   if (header.classList.contains('open')) {
     header.classList.remove('open');
-    overlay.classList.remove('f-in');
-    overlay.classList.add('f-out');
+    body.classList.remove('.noscroll');
+    fadings.forEach(function(element){
+      element.classList.remove('f-in');
+      element.classList.add('f-out');
+    });
+    
   } else {
+    body.classList.add('.noscroll');
     header.classList.add('open');
     fadings.forEach(function(element){
       element.classList.remove('f-out');
