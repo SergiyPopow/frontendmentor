9234feadcbc460db79a35a54ef6d3338a7b002ae
Serhii <malgothsm@gmail.com>
2021-01-11 16:18:31 +0200

    lixed style bug

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -3,7 +3,7 @@ const header = document.querySelector('.header');
 
 bttHamb.addEventListener('click', function() {
   if (header.classList.contains('open')) {
-    bheader.classList.remove('open');
+    header.classList.remove('open');
   } else {
     header.classList.add('open');
   }
